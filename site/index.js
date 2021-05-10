aa735d2555c028f8d10b6a34393059f362594d55
Veronica Braga <veronica.braga@a.ficr.edu.br>
2021-05-10 20:41:48 -0300

    teste3

--- site/index.js
+++ site/index.js
@@ -1,5 +1,18 @@
 var index = 555;
 
-i (index == 5) {
+if (index == 5) {
     consolelog("oii");
 } 
+
+var nome = "Projeto EPA"
+
+if (nome == "professor") {
+	consolelog("Bem vindo professor!!!");
+} else {
+	consolelog("Bem vindo aluno!!!")
+}
+
+var materia = ["Matemática", "Português", "Inglês", "História"];
+
+consolelog(materia);
+
