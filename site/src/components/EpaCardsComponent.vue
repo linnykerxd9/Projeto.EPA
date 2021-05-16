23bca1eb765901b0428cc95d10b6f6e3b345a2b4
simone-santos <simonepsantos014@gmail.com>
2021-05-16 14:15:31 -0300

    criando o componente de card

---
+++ site/src/components/EpaCardsComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="divCard">
+  	 <q-card class="my-card">
+      <img :src="imagem">
+      <q-card-section class="q-pt-none">
+        {{ descricao }}
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'EpaCardsComponent',
+   	props:{
+   	imagem:{
+   		type:String
+   	},
+   	descricao:{
+   		type:String
+   	}
+}
+
+}
+</script>
+
+<style>
+.divCard{
+	border: 3px solid rgb(71, 106, 74);
+	margin-left: 50px;
+	width: 311px;
+	height: 254px;
+	margin-right: 50px; 
+}
+.my-card{
+  border-radius: 0px;
+	background-color:rgb(71, 106, 74)!important;
+	box-shadow: none;
+
+}
+</style>
--- site/src/pages/Index.vue
+++ site/src/pages/Index.vue
@@ -1,14 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+  
+  <div class="row">
+   	<div>
+   	<EpaCardsComponent 
+  	imagem="img/pencil-918449_1920.png"
+  	descricao="Encontre a disciplima que você procura e um professor que lhe agrader perto de você"
+  ></EpaCardsComponent>
+  	</div>
+
+  	<div>
+  	<EpaCardsComponent 
+  	imagem="img/element5-digital-OyCl7Y4y0Bk-unsplash.png"
+  	descricao="Dê aulas perto de casa ou do trabalho, encontrando alunos nas redondezas "
+  ></EpaCardsComponent>
+  	</div>
+  </div>
 </template>
 
 <script>
+	import EpaCardsComponent from '../components/EpaCardsComponent.vue'
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+  	EpaCardsComponent
+  }
 }
 </script>
+<style>
+	.row{
+
+	}
+</style>
