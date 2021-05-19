276543d92592a09e60a020604d67d5cecff6877b
Mirla-Vieira <mirla.v.oliveira@gmail.com>
2021-05-19 12:52:24 -0300

    acrescentando algumas fotos e o início da navbar

--- site/src/layouts/MainLayout.vue
+++ site/src/layouts/MainLayout.vue
@@ -1,76 +1,73 @@
+
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh lpR fff">
+
+    <q-header class="header text-white">
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          <img src="img/marca.png" alt="EPA">
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      bordered
-      content-class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksData = [
-  {
-    title: 'professor',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: '/#/cadastro/professor'
-  },
-  {
-    title: 'aluno',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: '/#/cadastro/professor'
-  },
-];
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
   data () {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData
+     
     }
   }
 }
 </script>
+<style >
+ .header {
+    width: 100%;
+    background:#6BB1BB;
+    top: 0;
+    position: fixed;
+    padding: 15px 0;
+
+}
+
+.header_menu {
+    text-align: right;
+
+}
+
+.header_menu ul li {
+    
+    display: inline-block;
+    margin-left: 25px;
+    margin-top: 25px;
+}
+
+
+.header_menu ul li a{
+   font-family: Arial, Helvetica, sans-serif;
+   color: #000; 
+    font-weight: bold;
+    text-transform: uppercase;
+    letter-spacing: .1em;
+    font-size: 14px;
+}
+</style>
