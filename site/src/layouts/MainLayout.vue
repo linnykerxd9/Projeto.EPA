b84f835c46190b9b2190bef4fbfd067496e0db6b
Mirla-Vieira <mirla.v.oliveira@gmail.com>
2021-05-24 10:34:53 -0300

    adicionando parte da navbar

--- site/src/layouts/MainLayout.vue
+++ site/src/layouts/MainLayout.vue
@@ -3,10 +3,19 @@
   <q-layout view="hHh lpR fff">
 
     <q-header class="header text-white">
-      <q-toolbar>
+      <q-toolbar  class="container">
         <q-toolbar-title>
-          <img src="img/marca.png" alt="EPA">
+          <img src="img/logo_epa.png" alt="EPA" class="imagem grid-4">
         </q-toolbar-title>
+<div class="header_menu grid-12">
+  <ul>
+    <li><a href="#">Sobre</a></li>
+    <li><span class="dot"></span><a href="#">Como Começar</a></li>
+    <li><span class="dot"></span><a href="#">Contato</a></li>
+    <li><span class="dot"></span><a href="#">Login</a></li>
+  </ul>
+</div>
+
       </q-toolbar>
     </q-header>
 
@@ -45,12 +54,13 @@ export default {
     background:#6BB1BB;
     top: 0;
     position: fixed;
-    padding: 15px 0;
+    padding: 5px 0;
 
 }
 
 .header_menu {
     text-align: right;
+    
 
 }
 
@@ -59,6 +69,7 @@ export default {
     display: inline-block;
     margin-left: 25px;
     margin-top: 25px;
+    list-style-type: disc;
 }
 
 
@@ -69,5 +80,27 @@ export default {
     text-transform: uppercase;
     letter-spacing: .1em;
     font-size: 14px;
+    text-decoration: none;
+}
+
+.container {
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
+.dot {
+  height: 8px;
+  width: 8px;
+  background-color: #000;
+  border-radius: 50%;
+  display: inline-block;
+  padding-right:8px;
+  margin-right: 15px;
+
+}
+
+.imagem {
+  margin-top: 15px;
 }
 </style>
