a03b53480395cc28f6f016cf2d934fd49c40da93
linnykerxd9 <linnykerxd9@gmail.com>
2021-06-04 19:42:26 -0300

    Adicionando os professores ao mapa e mostrando o perfil deles

--- site/src/components/EpaMapaComponent.vue
+++ site/src/components/EpaMapaComponent.vue
@@ -1,18 +1,47 @@
 <template>
-
+<section>
    <GmapMap
   :center="{lat:currentLocation.lat, lng:currentLocation.lng}"
   :zoom="14"
   map-type-id="roadmap"
   style="width: 100%; height: 450px;"
 >
+  <GmapMarker
+    v-for="(localizacao, index) in localizacoes"
+    :key="index.id"
+    :position="getPosition(localizacao)"
+    :clickable="true"
+    @click="mostrarPerfil(localizacao.idProfessor)"
+  />
 </GmapMap>
 
+    <q-dialog v-model="perfil" v-if="professor != null">
+      <q-card class="my-card">
+        <q-img src="img/foto-perfil.jpg" />
+
+        <q-card-section>
+          <div class="row no-wrap items-center">
+            <div class="col text-h6 ellipsis" style="margin-top:10px;">
+               {{professor.nome_completo}}
+            </div>
+          </div>
+          <q-rating v-model="stars" :max="5" size="32px" />
+        </q-card-section>
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1" >
+              Telefone: {{professor.telefone}}
+          </div>
+        </q-card-section>
+        <q-separator />
+        <q-card-actions align="right">
+          <q-btn v-close-popup flat color="primary" label="Chat" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+</section>
 </template>
 
 <script>
-// import {load, Map, Marker} from 'vue-google-maps'
- // load('AIzaSyD-xjGEnuK7hBDolTVmQFPL8mt3D1lJ_z0')
 import {gmapApi} from 'vue2-google-maps'
 import { server } from 'boot/axios'
 import Vue from 'vue'
@@ -29,7 +58,10 @@ export default {
   data () {
     return {
         currentLocation : { lat: -34.397, lng: 150.644},
-        localizacoes: [],
+        localizacoes:  [],
+        professor: null,
+        perfil:false,
+        stars: 3,
     }
   },
   methods: {
@@ -41,22 +73,48 @@ export default {
         };
       });
     },
-   mostrarProfs() {
-     server.get('localizacao')
-     .then(localizacoes => {
-       this.localizacoes = localizacoes.data
-       console.log(this.localizacoes);
-       })
-     .catch(err => console.log(err))
-   }
+
+     mostrarProfs() {
+       server.get('localizacao')
+      .then(localizacoes => {
+        this.localizacoes = localizacoes.data.filter( localizacao =>   localizacao.situacao == 1);
+        }).catch(() => {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'Falha ao procurar por professores próximos'
+        })
+      })
+    },
+     getPosition(marker) {
+      return {
+        lat: parseFloat(marker.latitude),
+        lng: parseFloat(marker.longitude)
+      }
   },
+  mostrarPerfil(idProfessor){
+      server.get(`professor/${idProfessor}`)
+      .then(professor => {
+        this.professor = professor.data[0];
+        this.perfil = true;
+      })
+      .catch(() => {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'Falha ao acessar esse perfil'
+        })
+      })
+  }
+},
    computed: {
     google: gmapApi,
   },
   mounted() {
     this.localizar();
     this.mostrarProfs();
- }
-
+ },
 }
 </script>
