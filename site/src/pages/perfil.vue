97df07b3b96a6ca09185dab493c0298edee1021a
Mirla-Vieira <mirla.v.oliveira@gmail.com>
2021-06-11 19:10:01 -0300

    finalizando o perfil

--- site/src/pages/perfil.vue
+++ site/src/pages/perfil.vue
@@ -5,7 +5,7 @@
         <div class="column center">
           <div>
              <q-avatar size='150px'>
-                <img src="https://cdn.quasar.dev/img/avatar.png">
+                <img src="img/foto-perfil.jpg">
               </q-avatar>
           </div>
           <div class="perfilContainer">
@@ -21,31 +21,40 @@
                       v-model="usuario.nome_completo"
                       label="Seu nome Completo *"
                       lazy-rules
-                      :rules="[
-                      val => val && val.length > 0 || 'O campo não pode ser nulo',]"
+                      :rules="[val => val && val.length > 0 || 'O campo não pode ser nulo',]"
                     />
                   </div>
-                  <div class="q-pa-md sobreVoce"  style="max-width: 300px">
+
+                  <div class="sobreVoce"  style="margin-top: 0px;">
                       <q-input
                         v-model="usuario.sobre"
                         filled
                         autogrow
                         label="Escreva um pouco sobre você"
+                        lazy-rules
+                      :rules="[val => val && val.length > 0 || 'O campo não pode ser nulo',]"
                       />
                     </div>
-                    <div class="minhasMaterias">
-                      <h4>Minhas Matérias</h4>
-                      <div class="minhasMateriasContent">
+                    
+                  </div>
+                    <div class="minhasMaterias container" v-if="professor">
+                      <h4 style="margin:20px 0px;">Minhas Matérias</h4>
+                      <div class="minhasMateriasContent" v-if="materias.length != 0">
+                        <div class="materiasColumn">
                          <materiasComponent
-                                      nome="alguma"
-                                      serie="coisa"
-                                      valor="alguma"
-                                      idMateria="coisa"
-                                      tipo="alguma"
+                                      v-for="materia in materias"
+                                      :key="materia.id"
+                                      :nome="materia.Materium.nome"
+                                      :serie="materia.Materium.serie"
+                                      :valor="materia.Materium.valorMateria"
+                                      :tipo="materia.Materium.escolaridade"
                      ></materiasComponent>
+                        </div> 
+                      </div>
+                       <div class="center" v-else>
+                           <h6 style="color:#828080;">Nenhuma matéria cadastrada</h6>
                       </div>
                     </div>
-                  </div>
 
 
 
@@ -54,9 +63,8 @@
 
 
 
-
-              <div class="btnCadastro">
-                <q-btn label="Atualizar" type="submit" color="primary"  :loading="enviando" />
+              <div class="btnAtualizar">
+                <q-btn label="Atualizar" type="submit" color="primary"  />
               </div>
            </q-form>
           </div>
@@ -67,7 +75,6 @@
 </template>
 
 <script>
-
 import { server } from 'boot/axios'
 import materiasComponent from '../components/materiasComponent.vue'
 export default {
@@ -75,13 +82,61 @@ export default {
    components:{ materiasComponent },
    data(){
      return {
+      idUsuario: this.$route.params.id.split('-'),
+      professor: false,
       usuario: {
         nome_completo:null,
         sobre:null,
-      
        },
         materias:[],
      }
+   },
+   methods: {
+     minhasMaterias () {
+      if (this.idUsuario[0] == 'PRO') {
+        this.professor = true;
+        console.log(this.idUsuario);
+        server.get(`materiaProf/${this.$route.params.id}`)
+      .then(materia => this.materias = materia.data)
+      .catch(()=>{
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'Falha ao recuperar suas matérias'
+        })
+      });
+      }
+     },
+     onSubmit(){
+      if (this.idUsuario[0]='PRO') {
+        server.put(`professor/${this.$route.params.id}`,this.usuario)
+        .then(professor=> this.$q.sessionStorage.set("usuario", this.usuario))
+        .catch(()=>{
+          this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'Falha ao recuperar suas matérias'
+        })
+      });
+      } else{
+         server.put(`aluno/${this.$route.params.id}`,this.usuario)
+        .then(professor=> this.$q.sessionStorage.set("usuario", this.usuario))
+        .catch(()=>{
+          this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'Falha ao recuperar suas matérias'
+        })
+      });
+      }
+     },
+   },
+   beforeMount() {
+     this.usuario = this.$q.sessionStorage.getItem('usuario');
+     this.minhasMaterias(); 
    }
 }
 </script>
@@ -100,4 +155,29 @@ export default {
   justify-content: center;
   align-items: center;
 }
+
+#sectionPerfil .nomeCompleto, #sectionPerfil .sobreVoce {
+  width: 300px;
+  margin: 40px 0px;
+
+}
+#sectionPerfil .minhasMateriasContent {
+ width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+#sectionPerfil .materiasColumn {
+  min-width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-self: center; 
+}
+#sectionPerfil  .btnAtualizar {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 </style>
